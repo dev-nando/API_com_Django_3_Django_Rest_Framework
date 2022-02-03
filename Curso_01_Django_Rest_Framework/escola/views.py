847340a5c20b01f58baa23a31eb85aebaa6620ab
mac_02 <mac_02@email.com>
2022-02-03 13:12:08 -0300

    Requisição GET

---
+++ Curso_01_Django_Rest_Framework/escola/views.py
@@ -0,0 +1,7 @@
+import imp
+from django.http import JsonResponse
+
+def alunos(request):
+    if request.method == 'GET':
+        aluno = { 'id': 1, 'nome': 'Guilherme' }
+        return JsonResponse(aluno)
