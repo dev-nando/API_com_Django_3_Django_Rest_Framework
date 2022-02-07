88f3acd0e213a2f6a87381b52b77962ceb2a7efa
mac_02 <mac_02@email.com>
2022-02-07 13:09:19 -0300

    Viewset

--- Curso_01_Django_Rest_Framework/escola/views.py
+++ Curso_01_Django_Rest_Framework/escola/views.py
@@ -1,7 +1,18 @@
+from ctypes import DllCanUnloadNow
 import imp
-from django.http import JsonResponse
+from re import A
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = { 'id': 1, 'nome': 'Guilherme' }
-        return JsonResponse(aluno)
+class AlunosViewSer(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursisViewSer(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
