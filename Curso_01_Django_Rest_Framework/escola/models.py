a4dabde5fee9d3e11419a0271bc7234737fb4c0e
mac_02 <mac_02@email.com>
2022-02-07 15:36:49 -0300

    Modelo de matrícula

--- Curso_01_Django_Rest_Framework/escola/models.py
+++ Curso_01_Django_Rest_Framework/escola/models.py
@@ -10,7 +10,7 @@ class Aluno(models.Model):
     cpf = models.CharField(max_length=11)
     data_nascimento = models.DateField()
 
-    def ___str__(self):
+    def __str__(self) -> str:
         return self.nome
 
 class Curso(models.Model):
@@ -26,3 +26,13 @@ class Curso(models.Model):
 
     def __str__(self) -> str:
         return self.descricao
+
+class Matricula(models.Model):
+    PERIODO = (
+        ( 'M', 'Matutino' ),
+        ( 'V', 'Vespertino' ),
+        ( 'N', 'Noturno' )
+    )
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    periodo = models.CharField(max_length=1, choices=PERIODO, blank=False, null=False, default='M')
