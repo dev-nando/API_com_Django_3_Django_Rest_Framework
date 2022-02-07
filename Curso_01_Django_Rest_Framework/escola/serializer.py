e538fe714813322ba05bfc31c1104ad41359670b
mac_02 <mac_02@email.com>
2022-02-07 13:01:27 -0300

    Serializers

---
+++ Curso_01_Django_Rest_Framework/escola/serializer.py
@@ -0,0 +1,13 @@
+from django.forms import fields_for_model
+from rest_framework import serializers
+from escola.models import Aluno, Curso
+
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = [ 'id', 'nome', 'rg', 'cpf', 'data_nascimento' ]
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = '__all__'
