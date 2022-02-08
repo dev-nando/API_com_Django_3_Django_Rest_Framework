c84f56a3bf5ae063f63da91b512c557fbd103b2a
fer <->
2022-02-08 00:16:47 -0300

    Validando todos os campos

--- Curso_02_Validacoes_buscas_filtros_e_deploy/clientes/serializers.py
+++ Curso_02_Validacoes_buscas_filtros_e_deploy/clientes/serializers.py
@@ -8,4 +8,16 @@ class ClienteSerializer(serializers.ModelSerializer):
     def validate_cpf(self, cpf):
         if len(cpf) != 11:
             raise serializers.ValidationError("O CPF deve ter 11 digitos!")
-        return cpf    
+        return cpf
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError("Não inclua números nesse campo!")
+        return nome
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("O RG deve ter 9 digitos!")
+        return rg
+    def validate_celular(self, celular):
+        if len(celular) != 11:
+            raise serializers.ValidationError("O celular deve ter 11 digitos!")
+        return celular
